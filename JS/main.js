d0c52b236e224b47c687f5e5a54b7fe06acb8ca1
JooChan <14jshin@gmail.com>
2018-02-07 23:56:53 -0500

    boxes now animate

--- JS/main.js
+++ JS/main.js
@@ -18,8 +18,12 @@ function onload_(){
 		document.getElementsByClassName("shape2")[i].style.marginLeft = ""+getRandomInt(2000)+"px"; 
 
 		document.getElementById("triangle").style.animation = "fadeIn 1s ease-in";
+		document.getElementById("square1").style.animation = "rotateL 1s ease-in-out infinite alternate, fadeIn 1s ease-in";
+		document.getElementById("square2").style.animation = "rotateR 1s ease-in-out infinite alternate, fadeIn 1s ease-in";
 		setTimeout(function(){
 			document.getElementById("triangle").style.animation = "none";
+			document.getElementById("square1").style.animation = "rotateL 1s ease-in-out infinite alternate";
+			document.getElementById("square2").style.animation = "rotateR 1s ease-in-out infinite alternate";
 		}, 1000);
 	}
 }
@@ -53,5 +57,15 @@ $(document).ready(function(){
         $(this).css("animation", "_hoverTIn_ 100s ease");
         }, function(){
         $(this).css("animation", "_hoverTOut_ 100s ease");
+    });
+    $("#square1").hover(function(){
+        $(this).css("animation", "rotateL 1s ease-in-out infinite alternate, _hoverCIn_ 100s ease");
+        }, function(){
+        $(this).css("animation", "rotateL 1s ease-in-out infinite alternate, _hoverCOut_ 100s ease");
+    });
+     $("#square2").hover(function(){
+        $(this).css("animation", "rotateR 1s ease-in-out infinite alternate, _hoverCIn_ 100s ease");
+        }, function(){
+        $(this).css("animation", "rotateR 1s ease-in-out infinite alternate, _hoverCOut_ 100s ease");
     });
 });
